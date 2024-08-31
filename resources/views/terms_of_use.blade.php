fad1dc3913b009b9262ea15e066ffc2def0e8c60
kimkundad <kim.kundad@gmail.com>
2024-08-31 17:23:30 +0700

    kim

--- resources/views/terms_of_use.blade.php
+++ resources/views/terms_of_use.blade.php
@@ -5,6 +5,24 @@
 @stop
 
 @section('stylesheet')
+
+<style>
+
+.terms .thead {
+        font-size: 18px;
+    color: #0984e3;
+    margin-bottom: 2px;
+}
+.terms p {
+    margin-bottom: 2px;
+    color: #666
+}
+.l-25{
+    padding-left:25px;
+    font-size: 14px
+}
+</style>
+
 @stop('stylesheet')
 
 @section('content')
@@ -12,13 +30,15 @@
 
 
 
-<div id="blog-page" class="blog-section section-padding">
+<div id="blog-page" class="blog-section section-padding" style="padding: 20px 0 80px 0;">
         <div class="container">
             <div class="row" style="justify-content: center;">
-                <div class="col-lg-8">
-                    <div class="single-blog-wrap">
+                <div class="col-lg-12">
+                    <div class="single-blog-wrap terms">
 
-
+                    <h3>เงื่อนไขการใช้งาน</h3>
+                    <p style="line-height:20px; font-size:14px">เมื่อท่านสมัครเข้าเป็นสมาชิกเคอีเอ็กซ์ คลับ ของบริษัท เคอีเอ็กซ์ เอ็กซ์เพรส (ประเทศไทย) จำกัด (มหาชน) (“บริษัทฯ” หรือ “เรา”) ผู้สมัครตกลงและยินยอมเข้าผูกพันตนตามข้อกำหนดและเงื่อนไขการเป็นสมาชิกเคอีเอ็กซ์ คลับ (“เงื่อนไขการเป็นสมาชิก”) ทั้งนี้ ผู้สมัครมีหน้าที่ทําความเข้าใจอย่างละเอียดก่อนสมัครสมาชิกหรือให้ข้อมูลอื่นใดแก่บริษัทฯ รวมถึงคอยตรวจสอบเงื่อนไขการให้บริการนี้อย่างสม่ำเสมอ หากท่านมีข้อสงสัยหรือคำถามเพิ่มเติมเกี่ยวกับการเป็นสมาชิกเคอีเอ็กซ์ คลับ สามารถสอบถามได้ผ่านช่องทาง Call center 1217 หรือช่องทางอื่นใดตามที่บริษัทฯ กำหนด </p>
+                    <p class="l-25">1. “ลูกค้า” หมายถึง ผู้ส่งสินค้า หรือ ผู้ส่งของ</p>
 
 
                     </div>
