7e3898a24b833dfa4ba0a576a6bb80ef484d23a4
kimkundad <kim.kundad@gmail.com>
2025-03-21 14:07:10 +0700

    kim

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema; //Import Schema
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +17,10 @@ class AppServiceProvider extends ServiceProvider
     {
         //
      //  \URL::forceScheme('https');
+
+     if (config('app.env') !== 'local') { // ใช้เฉพาะ production หรือ staging
+        URL::forceScheme('https');
+        }
         Schema::defaultStringLength(191);
 
     }
